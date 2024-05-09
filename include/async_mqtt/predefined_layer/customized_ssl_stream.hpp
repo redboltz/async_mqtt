3def6837507c9dda1a9a4b35d83d9a1567a9e119
Takatoshi Kondo <redboltz@gmail.com>
2024-05-09 18:23:03 +0900

    Fixed customization layer.
    Used async_compose to manage well associated properties.

--- include/async_mqtt/predefined_layer/customized_ssl_stream.hpp
+++ include/async_mqtt/predefined_layer/customized_ssl_stream.hpp
@@ -28,59 +28,75 @@ struct layer_customize<as::ssl::stream<NextLayer>> {
     >
     static auto
     async_close(
-        as::any_io_executor exe,
         as::ssl::stream<NextLayer>& stream,
         CompletionToken&& token
     ) {
-        return as::async_initiate<
+        return as::async_compose<
             CompletionToken,
             void(error_code const& ec)
         > (
-            [] (auto completion_handler,
-                as::any_io_executor exe,
-                as::ssl::stream<NextLayer>& stream
-            ) {
-                auto tim = std::make_shared<as::steady_timer>(
-                    exe,
-                    shutdown_timeout
-                );
-                auto ch_sp = std::make_shared<decltype(completion_handler)>(force_move(completion_handler));
-                tim->async_wait(
-                    // You must bind exe to the handler if you use underlying async_function
-                    // using the completion handler that you defined.
-                    as::bind_executor(
-                        exe,
-                        [wp = std::weak_ptr<as::steady_timer>(tim), ch_sp]
-                        (error_code const& ec) mutable {
-                            if (!ec) {
-                                if (auto sp = wp.lock()) {
-                                    ASYNC_MQTT_LOG("mqtt_impl", info)
-                                        << "TLS async_shutdown timeout";
-                                    force_move(*ch_sp)(ec);
-                                }
-                            }
-                        }
-                    )
-                );
-                stream.async_shutdown(
-                    // You must bind exe to the handler if you use underlying async_function
-                    // using the completion handler that you defined.
-                    as::bind_executor(
-                        exe,
-                        [tim, ch_sp]
-                        (error_code const& ec_shutdown) mutable {
-                            ASYNC_MQTT_LOG("mqtt_impl", info)
-                                << "TLS async_shutdown ec:" << ec_shutdown.message();
-                            force_move(*ch_sp)(ec_shutdown);
-                        }
-                    )
-                );
+            async_close_impl{
+                stream
             },
-            token,
-            exe,
-            std::ref(stream)
+            token
         );
     }
+
+    struct async_close_impl{
+        as::ssl::stream<NextLayer>& stream;
+
+        template <typename Self>
+        void operator()(
+            Self& self
+        ) {
+            auto tim = std::make_shared<as::steady_timer>(
+                stream.get_executor(),
+                shutdown_timeout
+            );
+            auto self_sp = std::make_shared<Self>(force_move(self));
+            tim->async_wait(
+                as::consign(
+                    as::append(
+                        std::ref(*self_sp),
+                        std::weak_ptr<as::steady_timer>(tim)
+                    ),
+                    self_sp
+                )
+            );
+            stream.async_shutdown(
+                as::consign(
+                    std::ref(*self_sp),
+                    self_sp,
+                    tim
+                )
+            );
+        }
+
+        template <typename Self>
+        void operator()(
+            Self& self,
+            error_code const& ec,
+            std::weak_ptr<as::steady_timer> wp
+        ) {
+            if (!ec) {
+                if (auto sp = wp.lock()) {
+                    ASYNC_MQTT_LOG("mqtt_impl", info)
+                        << "TLS async_shutdown timeout";
+                    self.complete(ec);
+                }
+            }
+        }
+
+        template <typename Self>
+        void operator()(
+            Self& self,
+            error_code const& ec
+        ) {
+            ASYNC_MQTT_LOG("mqtt_impl", info)
+                << "TLS async_shutdown ec:" << ec.message();
+            self.complete(ec);
+        }
+    };
 };
 
 } // namespace async_mqtt
