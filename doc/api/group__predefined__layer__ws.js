94a986f4307038deb96cbb4e8aad1e1f5f088a9f
Takatoshi Kondo <redboltz@gmail.com>
2024-05-19 01:09:18 +0900

    Updated documents.

--- doc/api/group__predefined__layer__ws.js
+++ doc/api/group__predefined__layer__ws.js
@@ -1,6 +1,6 @@
 var group__predefined__layer__ws =
 [
     [ "async_mqtt::protocol::ws", "group__predefined__layer__ws.html#gafa0029e1fe1da9ffc3e87e073cc1930c", null ],
-    [ "async_mqtt::underlying_handshake", "group__predefined__layer__ws.html#ga2e9378dc2e23d0dd06b4f970db49025a", null ],
-    [ "async_mqtt::underlying_handshake", "group__predefined__layer__ws.html#ga5b4d9505b919d29eae931ad73952cd95", null ]
+    [ "async_mqtt::async_underlying_handshake", "group__predefined__layer__ws.html#gaa852d19bb02f77d8603eeddf89ba18df", null ],
+    [ "async_mqtt::async_underlying_handshake", "group__predefined__layer__ws.html#ga24f02ea51005c77468c7f02df4b84d89", null ]
 ];
