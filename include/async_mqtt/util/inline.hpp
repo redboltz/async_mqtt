87bce404918508bacfe8f5c0579e0157a5b6230c
Takatoshi Kondo <redboltz@gmail.com>
2024-08-05 11:59:02 +0900

    Removed invalid `#` from inline.hpp.

--- include/async_mqtt/util/inline.hpp
+++ include/async_mqtt/util/inline.hpp
@@ -1,4 +1,4 @@
-#// Copyright Takatoshi Kondo 2024
+// Copyright Takatoshi Kondo 2024
 //
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
