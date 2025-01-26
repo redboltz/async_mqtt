aa3ceadecc49d0165e416f73362918577495a774
Takatoshi Kondo <redboltz@gmail.com>
2025-01-26 22:16:14 +0900

    Added unmatched sub/unsub.

--- test/unit/ut_ep_size_max.cpp
+++ test/unit/ut_ep_size_max.cpp
@@ -267,6 +267,146 @@ BOOST_AUTO_TEST_CASE(client_send_no_store) {
     th.join();
 }
 
+BOOST_AUTO_TEST_CASE(client_send_store) {
+    auto version = am::protocol_version::v5;
+    as::io_context ioc;
+    auto guard = as::make_work_guard(ioc.get_executor());
+    std::thread th {
+        [&] {
+            ioc.run();
+        }
+    };
+
+    auto ep = am::endpoint<async_mqtt::role::client, async_mqtt::stub_socket>{
+        version,
+        // for stub_socket args
+        version,
+        ioc.get_executor()
+    };
+
+    ep.next_layer().set_close_checker(
+        [&] { BOOST_TEST(false); }
+    );
+
+    auto connect1 = am::v5::connect_packet{
+        true,   // clean_start
+        0x1234, // keep_alive
+        "cid1",
+        std::nullopt, // will
+        "user1",
+        "pass1",
+        am::properties{
+            am::property::session_expiry_interval{0x0fffffff}
+        }
+    };
+
+    auto connack1 = am::v5::connack_packet{
+        false,   // session_present
+        am::connect_reason_code::success,
+        am::properties{
+            am::property::maximum_packet_size{21}
+        }
+    };
+
+    auto prepare =
+        [&](auto connect, auto connack) {
+            ep.next_layer().set_recv_packets(
+                {
+                    // receive packets
+                    {connack},
+                }
+            );
+
+            // underlying handshake
+            {
+                auto [ec] = ep.async_underlying_handshake(as::as_tuple(as::use_future)).get();
+                BOOST_TEST(!ec);
+            }
+
+            // send connect
+            ep.next_layer().set_write_packet_checker(
+                [&](am::packet_variant wp) {
+                    BOOST_TEST(connect == wp);
+                }
+            );
+            {
+                auto [ec] = ep.async_send(connect, as::as_tuple(as::use_future)).get();
+                BOOST_TEST(!ec);
+            }
+
+            // recv connack
+            {
+                auto [ec, pv] = ep.async_recv(as::as_tuple(as::use_future)).get();
+                BOOST_TEST(!ec);
+                BOOST_TEST(connack == *pv);
+            }
+        };
+
+    prepare(connect1, connack1);
+
+    // size: 21bytes
+    auto [ec1, pid1] = ep.async_acquire_unique_packet_id(as::as_tuple(as::use_future)).get();
+    BOOST_TEST(!ec1);
+    auto publish_1_q1 = am::v5::publish_packet(
+        pid1,
+        "topic1",
+        "payload1",
+        am::qos::at_least_once,
+        am::properties{}
+    );
+
+    // size: 22bytes
+    auto [ec2, pid2] = ep.async_acquire_unique_packet_id(as::as_tuple(as::use_future)).get();
+    BOOST_TEST(!ec2);
+    auto publish_2_q1 = am::v5::publish_packet(
+        pid2,
+        "topic1",
+        "payload1+",
+        am::qos::at_least_once,
+        am::properties{}
+    );
+
+    // send publish_1
+    ep.next_layer().set_write_packet_checker(
+        [&](am::packet_variant wp) {
+            BOOST_TEST(publish_1_q1 == wp);
+        }
+    );
+    {
+        auto [ec] = ep.async_send(publish_1_q1, as::as_tuple(as::use_future)).get();
+        BOOST_TEST(!ec);
+    }
+
+    // close
+    ep.next_layer().set_close_checker({});
+    ep.async_close(as::as_tuple(as::use_future)).get();
+
+    auto connect2 = am::v5::connect_packet{
+        false,   // clean_start
+        0x1234, // keep_alive
+        "cid1",
+        std::nullopt, // will
+        "user1",
+        "pass1"
+    };
+
+    auto connack2 = am::v5::connack_packet{
+        false,   // session_present
+        am::connect_reason_code::success,
+        am::properties{
+            am::property::maximum_packet_size{20}
+        }
+    };
+
+    prepare(connect2, connack2);
+
+    // close
+    ep.next_layer().set_close_checker({});
+    ep.async_close(as::as_tuple(as::use_future)).get();
+    guard.reset();
+    th.join();
+}
+
 BOOST_AUTO_TEST_CASE(client_recv) {
     auto version = am::protocol_version::v5;
     as::io_context ioc;
